8e45dbf44fc5285ef949272351d645482e712ad3
Lee-kangchan <lsc4237@gmail.com>
2021-01-18 22:09:39 +0900

    콜라츠 추측

---
+++ src/programers/level1/solution28.java
@@ -0,0 +1,24 @@
+package programers.level1;
+
+//콜라츠 추측
+public class solution28 {
+    public int solution(int num) {
+        int answer = 0;
+        long t = num;
+        while(t!=1){
+            if(answer == 500){
+                answer = -1;
+                break;
+            }
+            if(t%2==0){
+                t = t/2;
+            }else{
+                t = (t*3 )+1;
+            }
+            answer++;
+
+
+        }
+        return answer;
+    }
+}
