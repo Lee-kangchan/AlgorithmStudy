68610914eec0c2db2fb7d663bad989aba3583866
Lee-kangchan <lsc4237@gmail.com>
2021-01-18 22:06:58 +0900

    에라토스테네스의 체를 이용한 소수찾기

---
+++ src/programers/level1/solution14.java
@@ -0,0 +1,27 @@
+package programers.level1;
+
+// 에라토스테네스의 체를 사용한 소수찾기 - 최적의 알고리즘
+// 나자신의 배수들을 배열에 사용했다고 체크
+// 체크안한것들은 이제 소수가 됨
+public class solution14 {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n];
+        for(int i = 2 ; i < n; i++){
+            if(arr[i] == 1){
+                continue;
+            }
+            for(int j = i+ i  ; j < arr.length; j+=i){
+                   arr[j-1] = 1;
+            }
+        }
+
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i]==0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+}
