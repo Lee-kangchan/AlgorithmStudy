35f1e83f40e9930d89a41befd3a21100c3a1ac2e
Lee-kangchan <lsc4237@gmail.com>
2021-01-28 21:46:10 +0900

    이진 변환 반복하기

---
+++ src/programers/level2/challenge_season1_1.java
@@ -0,0 +1,27 @@
+package programers.level2;
+
+public class challenge_season1_1 {
+    //이진 변환 반복하기
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        String binary = "1";
+
+        int count = 0;
+        int Zero = 0;
+        while(!s.equals("1")&&!s.equals("0")){
+            binary = "";
+            for(int i = 0 ; i < s.length() ; i++){
+                if(s.charAt(i) == '1'){
+                    binary += "1";
+                }else{
+                    Zero++;
+                }
+            }
+            s = Integer.toBinaryString(binary.length());
+            count++;
+        }
+        answer[0] = count;
+        answer[1] = Zero;
+        return answer;
+    }
+}
