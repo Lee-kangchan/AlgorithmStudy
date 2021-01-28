3f1d477a549fd5f6f4d962774680a594ece81f76
Lee-kangchan <lsc4237@gmail.com>
2021-01-28 21:44:37 +0900

    소수 만들기

---
+++ src/programers/level2/summer_winter_coding2018_2.java
@@ -0,0 +1,51 @@
+package programers.level2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+//소수 만들기
+public class summer_winter_coding2018_2 {
+    public int solution(int[] nums) {
+        int answer = -1;
+        String data = "";
+        ArrayList<Integer> list = new ArrayList<>();
+        solve(list, nums, 0, 1, 2);
+
+        return list.size();
+    }
+    public void solve(ArrayList<Integer> set, int[] nums, int x,int y, int z){
+        int result = nums[x]+nums[y]+nums[z];
+
+        System.out.print(result + "  ");
+        if(check(result)){
+            set.add(result);
+        }
+        if(x == nums.length-3 && y == nums.length-2 && z == nums.length - 1){
+            return;
+        }
+        else if(y == nums.length-2 && z == nums.length - 1){
+            solve(set, nums, x+1, x+2, x+3);
+        }
+        else if(z== nums.length-1){
+            solve(set, nums, x, y+1, y+2);
+        }else{
+            solve(set, nums, x, y, z+1);
+        }
+
+
+
+    }
+    public boolean check(int num){
+        if(num >= 2){
+            for(int i=2; i < num; i++){
+                if(num%i==0){
+                    return false;
+                }
+            }
+        }else{
+            return false;
+        }
+        return true;
+    }
+}
