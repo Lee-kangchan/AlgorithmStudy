558631c4087e4da4cdbfd5bc1841ed6d14c2aed3
Lee-kangchan <lsc4237@gmail.com>
2021-02-11 21:01:36 +0900

    숫자 게임

---
+++ src/programers/level3/SummerWinter_coding2.java
@@ -0,0 +1,31 @@
+package programers.level3;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SummerWinter_coding2 {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int count = 0 ;
+        for(int i =0  ; i <A.length; i++){
+            if(count > A.length-1){
+                break;
+            }
+            while(count <= A.length-1){
+                if(A[i] < B[count] ){
+                    break;
+                }
+                count++;
+            }
+            if(count <= A.length -1){
+                if(A[i] < B[count]){
+                    answer++;
+                    count++;
+                }
+            }
+        }
+        return answer;
+    }
+}
