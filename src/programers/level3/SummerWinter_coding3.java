3d662278fb0358b4fd2ba01baf36175e1267abd3
Lee-kangchan <lsc4237@gmail.com>
2021-02-15 21:32:34 +0900

    기지국 설치

--- src/programers/level3/SummerWinter_coding3.java
+++ src/programers/level3/SummerWinter_coding3.java
@@ -1,53 +1,46 @@
 package programers.level3;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 //기지국 설치
 public class SummerWinter_coding3 {
     public int solution(int n, int[] stations, int w) {
-        boolean check[] = new boolean[n];
+
         int answer = 0;
-        for (int i : stations) {
-            for (int j = 0; j <= w; j++) {
-                if (i + j - 1 < n) {
-                    check[i + j] = true;
+        int len = w+w+1;
+        if(stations.length ==0){
+            return n/(len) + ((n%(len))/(len));
+        }
+        else{
+            for(int i = 0 ; i < stations.length; i++){
+                if(i == 0){
+                    if(stations[i] > w){
+                        int sum = stations[i] -w - 1;
+                        answer = sum%len == 0 ? answer + sum/len : answer + sum/len+1;
+                    }
+                }else{
+                    if(stations[i-1]+w < stations[i]-w){
+                        int sum = (stations[i]-w) - (stations[i-1]+w +1);
+                        answer = sum%len == 0 ? answer + sum/len : answer + sum/len+1;
+                    }
                 }
-                if (i - j - 1 >= 0) {
-                    check[i - j] = true;
+                if(i == stations.length-1){
+                    if(stations[i]+w < n){
+                        int sum = n - (stations[i]+w);
+                        answer = sum%len == 0 ? answer + sum/len : answer + sum/len+1;
+                    }
                 }
             }
         }
-        int count = 0;
-        while (count < check.length) {
-            if (!check[count]) {
-                int min = count, max = count, mid = 0;
-                answer++;
-                while (count < check.length ) {
-                    if(!check[count]) {
-                        max = count;
-                        count++;
-                    }else{
-                        break;
-                    }
-                }
-                mid = (max + min) / 2;
-                for (int k = 0; k <= w; k++) {
-                    if (mid + k - 1 < check.length && mid + k - 1 >= 0) {
-                        check[mid + k - 1] = true;
-                    }
-                    if (mid - k - 1 >= 0) {
-                        check[mid - k - 1] = true;
-                    }
-                }
-                count = 0 ;
-            } else {
-                count++;
-            }
-        }
-        return answer;
+        return answer  ;
     }
 
     public static void main(String[] args) {
         SummerWinter_coding3 summerWinter_coding3 = new SummerWinter_coding3();
-        System.out.println(summerWinter_coding3.solution(11, new int[]{4,3}, 1));
+        System.out.println(summerWinter_coding3.solution(11, new int[]{4,11}, 1));
         System.out.println(summerWinter_coding3.solution(16, new int[]{9}, 2));
     }
 }
