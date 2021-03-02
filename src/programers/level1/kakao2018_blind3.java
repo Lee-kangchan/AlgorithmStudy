1bf3c520e56fca876f7dda0761a431c66c166e28
Lee-kangchan <lsc4237@gmail.com>
2021-03-02 20:52:34 +0900

    압축

--- src/programers/level1/kakao2018_blind3.java
+++ src/programers/level1/kakao2018_blind3.java
@@ -6,24 +6,54 @@ import java.util.ArrayList;
 public class kakao2018_blind3 {
     public int[] solution(String msg) {
         int[] answer = {};
-        String c,n;
+        ArrayList<Integer> result = new ArrayList<>();
+        String current="", next=msg.charAt(0)+"";
         ArrayList<String> list = new ArrayList<>();
 
-        for(int i = 0 ; i < msg.length()-1 ; i++){
-            c = msg.charAt(i)+"";
-            int count = 0 ;
-            for(String s : list){
-                if(s.equals(msg.substring(i, i + s.length()))){
-                    c = msg.substring(i, i + s.length());
-                    count = s.length();
+        for(int i = 0 ; i <= 'Z'- 'A' ; i++){
+            list.add((char)('A'+i)+"");
+        }
+        int count = 0;
+        while(msg.length()>=count+1){
+            current += next;
+            if(count+1 < msg.length()) {
+                next = msg.charAt(count + 1) + "";
+            }else{
+                for(int j = 0 ; j<list.size();j++){
+                    if(list.get(j).equals(current)){
+                        result.add(j+1);
+                    }
                 }
+                list.add(current+next);
+
+                System.out.println(current);
+                current = "";
             }
-            i+=count;
+            count++;
+            if(list.contains(current+next)){
+                continue;
+            }else{
+                for(int j = 0 ; j<list.size();j++){
+                    if(list.get(j).equals(current)){
+                        result.add(j+1);
+                    }
+                }
+                list.add(current+next);
 
-
-
+                System.out.println(current);
+                current = "";
+            }
+        }
+        answer = new int[result.size()];
+        for(int i = 0 ; i < answer.length;i ++){
+            answer[i] = result.get(i);
+            System.out.print(result.get(i) + " " );
         }
         return answer;
     }
 
+    public static void main(String[] args) {
+        kakao2018_blind3  kakao = new kakao2018_blind3();
+        System.out.println(kakao.solution("AAAAAAA"));
+    }
 }
