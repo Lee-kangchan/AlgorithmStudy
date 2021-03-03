bef8d6e1cf55627988bb6a1a6e26c8726d0d953a
Lee-kangchan <lsc4237@gmail.com>
2021-03-03 12:57:22 +0900

    방금그곡

--- src/programers/level2/kakao2018_blind3.java
+++ src/programers/level2/kakao2018_blind3.java
@@ -13,11 +13,10 @@ public class kakao2018_blind3 {
             String time[] = ans[0].split(":");
             String time2[] = ans[1].split(":");
             int time3 = 60*(Integer.parseInt(time2[0]) - Integer.parseInt(time[0]))+ Integer.parseInt(time2[1]) - Integer.parseInt(time[1]);;
-            while(time3 >=  ans[3].length()){
+            while(time3 >  ans[3].length()){
                 ans[3]+=ans[3];
             }
-
-            for(int j=0; j< time3-m.length(); j++){
+            for(int j=0; j<= time3; j++){
                 if(m.equals(ans[3].substring(j,j+m.length()))){
                     if(j+m.length() != ans[3].length() && ans[3].charAt(j+m.length())=='#'){
 
@@ -31,7 +30,7 @@ public class kakao2018_blind3 {
             }
         }
         if(answer.equals("")){
-            answer = "None";
+            answer = "(None)";
         }
         return answer;
     }
