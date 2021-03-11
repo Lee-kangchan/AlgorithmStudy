8b1d3ab3026f82381700f6cd60d5e68c7ddcb31f
Lee-kangchan <lsc4237@gmail.com>
2021-03-11 14:31:49 +0900

    후보키

--- src/programers/level2/kakao2019_blind.java
+++ src/programers/level2/kakao2019_blind.java
@@ -1,63 +1,64 @@
 package programers.level2;
 
-import java.util.HashSet;
+import java.util.*;
 
 //후보키
 public class kakao2019_blind {
-    int t = 1;
-    public int solution(String[][] relation) {
-        for(String s : relation[0]){
-            t = t*2;
-        }
-        solve(relation, new boolean[relation.length], 1, relation.length, relation[0].length);
 
+    Comparator<Integer> comp = new Comparator<Integer>() {
+        int countBits(int n){
+            int ret = 0 ;
+            while(n!=0){
+                if((n&1) != 0) ++ ret;
+                n = n >>1;
+            }
+            return ret;
+        }
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            int x = countBits(o1), y= countBits(o2);
+            return x-y;
+
+        }
+    };
+    public int solution(String[][] relation){
+        int answer = 0;
+        int row = relation.length;
+        int cols = relation[0].length;
+        List<Integer> candidates = new LinkedList<>();
+
+        for(int i = 1 ; i < 1 << cols; ++i){
+            if(check(relation, row, cols, i)){
+                candidates.add(i);
+            }
+        }
+        Collections.sort(candidates, comp);
+
+        while(candidates.size() != 0) {
+            int n = candidates.remove(0);
+            ++answer;
+            for(Iterator<Integer> it = candidates.iterator(); it.hasNext();){
+                int c = it.next();
+                if((n & c) == n)
+                    it.remove();
+            }
+        }
         return answer;
     }
-    int answer = 0;
-    public void solve(String[][] relation, boolean[] check, int bit, int height, int width){
-        HashSet<Integer> set = bitmask(bit);
-
-        if(t == bit){
-            return;
-        }
-        for(int i : set){
-            if(check[i]){
-                solve(relation, check, bit+1, height, width);
-                return;
+    boolean check(String[][] relation, int row, int cols, int subset){
+        for( int a = 0 ; a < row-1 ; a++){
+            for(int b= a + 1 ; b < row;b++){
+                boolean check = true;
+                for(int k = 0; k < cols; k++){
+                    if((subset & 1 << k)==0) continue;
+                    if(!relation[a][k].equals(relation[b][k])){
+                        check = false;
+                        break;
+                    }
+                }
+                if(check) return false;
             }
         }
-        HashSet<String> s = new HashSet<>();
-        for(int i = 0 ; i < height; i++){
-            String data = "";
-            for(int j : set){
-                data += relation[i][j];
-            }
-            s.add(data);
-        }
-        if(s.size() == height){
-            answer++;
-            for(int j : set){
-                check[j] = true;
-            }
-        }
-
-        solve(relation, check, bit+1, height, width);
-    }
-    public HashSet<Integer> bitmask(int bit){
-        String data = Integer.toBinaryString(bit);
-        HashSet<Integer> set = new HashSet<>();
-        int count = data.length()-1;
-        for(String s : data.split("")){
-            if(s.equals("1")){
-                set.add(count);
-            }
-            count--;
-        }
-        return set;
-    }
-
-    public static void main(String[] args) {
-        kakao2019_blind kakao = new kakao2019_blind();
-        System.out.println(kakao.solution(new String[][]{{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}}));
+        return true;
     }
 }
