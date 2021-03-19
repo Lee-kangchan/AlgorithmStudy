57e28c3dfe61e86b5f94e89d87d0c887d463490b
Lee-kangchan <lsc4237@gmail.com>
2021-03-19 16:45:04 +0900

    징검다리건너기

--- src/programers/level3/징검다리건너기.java
+++ src/programers/level3/징검다리건너기.java
@@ -1,22 +1,41 @@
 package programers.level3;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 // 2019 카카오 개발자 겨울 인턴십 : 징검다리 건너기
 public class 징검다리건너기 {
     public int solution(int[] stones, int k) {
-        int sum = Integer.MAX_VALUE;
-        for(int i = 0 ; i <stones.length-k; i++){
-            int data=0;
+        int answer = 0;
+        int start = 1 ;
+        int end = Arrays.stream(stones).max().getAsInt();
 
-            if(sum <= stones[i]){
-                continue;
+        while(start < end){
+            int mid = (start+end)/2;
+            if(check(stones, k, mid)){
+                start = mid +1;
+                answer = mid;
+            }else {
+                end = mid -1;
+
             }
-            for(int j = 0 ; j< k; j++){
-                data = data > stones[i+j] ? data : stones[i+j];
-            }
-            sum = sum > data ? data : sum;
+
         }
-        return sum;
+
+        return answer;
+    }
+    public boolean check(int[] stones, int k, int c){
+        int cnt =0;
+        for(int i = 0 ; i <stones.length ;i++){
+            if(stones[i] < c){
+                cnt++;
+                if(cnt >= k){
+                    return false;
+                }
+            }else{
+                cnt = 0;
+            }
+        }
+        return true;
     }
 }
