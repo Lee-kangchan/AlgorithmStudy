09ca647b1c9e87ef78005e13c1fa82c946ca86a8
Lee-kangchan <lsc4237@gmail.com>
2021-05-04 22:07:50 +0900

    호텔방배정

--- src/programers/level4/호텔방배정.java
+++ src/programers/level4/호텔방배정.java
@@ -1,28 +1,38 @@
 package programers.level4;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 //2019 카카오 개발자 겨울 인턴십
 public class 호텔방배정 {
     public long[] solution(long k, long[] room_number) {
-        long[] answer = new long[room_number.length];
+        int len = room_number.length;
+        long[] answer = new long[len];
         HashMap<Long, Long> map = new HashMap<>();
-        for(int i = 0 ; i < room_number.length; i++){
+        for(int i = 0 ; i < len; i++){
            if(map.get(room_number[i]) == null){
                map.put(room_number[i] , room_number[i] + 1);
                answer[i] = room_number[i];
            }else{
                Long t = room_number[i];
                Long num = map.get(t);
+               List<Long> list = new ArrayList<>();
+               list.add(t);
+               list.add(num);
                while(true){
                    if(map.get(num)== null ){
-                       map.put(t, num);
-                       map.put(num, num+1);
+                       for(Long l : list){
+                           map.put(l, num+1);
+                       }
                        answer[i] = num;
                        break;
                    }else{
                        num = map.get(num);
+                       list.add(num);
+                       System.out.println(t);
+
                    }
                }
            }
