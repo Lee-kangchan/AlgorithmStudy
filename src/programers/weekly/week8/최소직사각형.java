d37f8b4be947b1cba9404825cb6fdef09d6f5899
이강찬 <lsc4237@gmail.com>
2021-10-01 17:04:12 +0900

    최소직사각형

---
+++ src/programers/weekly/week8/최소직사각형.java
@@ -0,0 +1,17 @@
+package programers.weekly.week8;
+
+import java.util.Arrays;
+
+public class 최소직사각형 {
+    public int solution(int[][] sizes) {
+        int len = sizes.length;
+        int x = 0;
+        int y = 0;
+        for(int i = 0 ; i < len ; i ++){
+            Arrays.sort(sizes[i]);
+            x = sizes[i][0] > x ? sizes[i][0] : x;
+            y = sizes[i][1] > y ? sizes[i][1] : y;
+        }
+        return x*y;
+    }
+}
