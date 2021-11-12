62cb72f001ac4b409d9af05227eabe8387c6d21c
이강찬 <lsc4237@gmail.com>
2021-11-12 21:46:58 +0900

    파도반 수열

--- src/baekjoon/dp/파도반수열.java
+++ src/baekjoon/dp/파도반수열.java
@@ -10,39 +10,32 @@ public class 파도반수열 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[]{1,1,1,2,2,3,4,5,7,9,12};
-        List<Integer> list =new ArrayList<>();
+        Long[] arr = new Long[102];
+        arr[1] = 1L;
+        arr[2] = 1L;
+        arr[3] = 1L;
+        arr[4] = 2L;
+        arr[5] = 2L;
 
-        for(int i = 0 ; i < arr.length; i++){
-            list.add(arr[i]);
+        for(int i = 6; i < 102; i++){
+            arr[i] = dp(i, arr);
         }
-
         int i = Integer.parseInt(br.readLine());
         int[] test = new int[i];
         for(int j = 0 ; j < i ; j ++){
             test[j] = Integer.parseInt(br.readLine());
-        }
-        for(int k = 0 ; k < test.length; k++) {
-            if (test[k]-1 <= list.size()-1) {
-                bw.write("" + list.get(test[k]-1));
-            } else {
-                for (int j = list.size(); j < test[k]; j++) {
-                    list.add(list.get(j - 1) + list.get(j - 5));
-                }
-
-                bw.write("" + list.get(test[k]-1));
-
-            }
+            bw.write(arr[test[j]]+"");
             bw.newLine();
         }
 
-
-        for(int a = 0 ; a < list.size();a++){
-            System.out.print(list.get(a)+"  ");
-        }
-        System.out.println();
         bw.flush();
         bw.close();
 
+    }
+    public static Long dp(int i, Long[] arr){
+        if(arr[i] != null){
+            return arr[i];
+        }
+        return dp(i-2,arr) + dp(i-3,arr);
     }
 }
